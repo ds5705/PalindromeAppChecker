dc30485b756942234681eb905a054b5e56fbccdd
ds5705 <ds5705@srmist.edu.in>
2026-03-06 09:43:14 +0530

    UC 10 IS DONE

---
+++ Palindrome/src/UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class UseCase10PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String normalized = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < normalized.length() / 2; i++) {
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+
+        sc.close();
+    }
+}
