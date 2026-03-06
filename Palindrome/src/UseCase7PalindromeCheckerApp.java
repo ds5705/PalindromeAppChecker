f83b83f5a42aa2cdf70e8f9e21d9fd9062bfb315
ds5705 <ds5705@srmist.edu.in>
2026-03-06 09:24:23 +0530

    UC 7 is done

---
+++ Palindrome/src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine().trim().toLowerCase();
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+
+        sc.close();
+    }
+}
